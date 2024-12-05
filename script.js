459f039edff10ef49ddb77187bee674fb9b746be
Rashika Jain <jainrashika174@gmail.com>
2024-12-05 22:34:19 +0530

    Add new js file

--- script.js
+++ script.js
@@ -1,76 +1,9 @@
-// calculate the score of user and computer and print the winner 
-function playGround(humanChoice , computerChoice)
-{
-    let computerScore = 0 ; 
-    let humanScore = 0 ; 
+let userSelectionContainer = document.querySelector("#container") ; 
 
-    for(let i = 0 ; i< 5 ; i++){
-        const humanChoiceInsensitive = humanChoice.toLowerCase() ; 
-        if(humanChoiceInsensitive=== computerChoice)
-        { 
-            console.log("It's a tie..But you played well!!") ; 
-        }
-        else if((humanChoiceInsensitive == "rock" && computerChoice == "paper") ||
-        (humanChoiceInsensitive== "scissors" && computerChoice == "rock") || 
-        (humanChoiceInsensitive == "paper" && computerChoice == "scissors"))
-        {
-            computerScore +=1 ; 
-            console.log("You lose!! You choses" , computerChoice  , "beats" , humanChoice) ; 
-        }
-        else
-        {
-            humanScore +=1 ; 
-            console.log("It's a Win..Hurray!! " , humanChoice , "beats" , computerChoice ) ; 
-        } 
-        if(i!==4)
-        {
-            humanChoice =     getHumanChoice()
-            computerChoice =   getComputerChoice() ; 
-        }
-    }
-
-        console.log("Your Score" , humanScore , "Computer Score" , computerScore) ; 
-        if(humanScore > computerScore)
-        {
-            console.log("Hurray , You win") ; 
-        }
-        else if(computerScore > humanScore){
-            console.log("You lose !! Try again") ; 
-        }
-        else{
-            console.log("It's a tie") ; 
-        }
-
-}
-
-// gets the value selected by computer
-function getComputerChoice()
-{
-    let noOfChoices = 3 ; 
-    let computerSelection ; 
-    let computerValue = Math.floor(Math.random() * noOfChoices) ;
-    if(computerValue === 0)
+userSelectionContainer.addEventListener("click", function(ev){
+    if(ev.target.tagName === "BUTTON")
     {
-        computerSelection = "rock" ; 
+    let userValue = ev.target.id ; 
+    console.log(userValue) ; 
     }
-    else if(computerValue === 1)
-    {
-        computerSelection = "paper"
-    }
-    else{
-        computerSelection = "scissors"
-    }
-    return computerSelection ; 
-}
-
-// gets the human input
-function getHumanChoice()
-{
-    return prompt("Enter : rock / paper / scissors") ;
-}
-
-
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
-
-playGround(humanSelection , computerSelection )
+})
